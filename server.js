a6736ac11ee2dfe5af4d849802a3379f282c6802
Benito Gonzalez <ohnobenito@gmail.com>
2020-12-13 10:27:26 -0600

    Setting up dependencies and express app

---
+++ server.js
@@ -0,0 +1,12 @@
+//Require Dependencies 
+const http = require("http");
+const fs = require("fs");
+const express = require("express");
+const path = require("path");
+
+//Setting up Express app
+const app = express();
+const port = 3030;
+
+
+
